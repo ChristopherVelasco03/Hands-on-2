b42c9b3fa1474177d81bdc81eb5d1bb63b2cfaa5
Christopher Antonio Velasco Gutierrez <155390541+ChristopherVelasco03@users.noreply.github.com>
2024-10-03 18:08:59 -0600

    Add files via upload

---
+++ Create/Create.bat
@@ -0,0 +1,21 @@
+@echo off
+echo Creando el archivo de texto mytext.txt...
+echo Hola Mundo > mytext.txt
+echo.
+echo Mostrando el contenido de mytext.txt:
+type mytext.txt
+echo.
+echo Creando el subdirectorio backup...
+mkdir backup
+echo.
+echo Copiando mytext.txt al subdirectorio backup...
+copy mytext.txt backup\
+echo.
+echo Listando el contenido del subdirectorio backup:
+dir backup\
+echo.
+echo Eliminando el archivo mytext.txt del subdirectorio backup...
+del backup\mytext.txt
+echo.
+echo Eliminando el subdirectorio backup...
+rmdir backup
